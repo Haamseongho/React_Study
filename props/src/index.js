109438388686e3246ab884affa69fca8d24999eb
haamseongho <haams704@gmail.com>
2017-12-23 02:59:31 +0900

    props 추가

---
+++ props/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import {ReactTest} from "./components/App";
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<ReactTest propName={"seongho"}>12월 크리스마스!</ReactTest>, rootElement);
+ReactDOM.render(<App/>, document.getElementById("test"));
+ReactDOM.render(<App value={0}></App>, Document.getElement("test2"));
+ReactDOM.render(<App secondValue={2}>기본 값은 1인데 설정은 2로함</App>, Document.getElement("test3"));
+ReactDOM.render(<App thirdValue={4}>기본 값은 2인데 설정은 4로함</App>, Document.getElement("test4"));
