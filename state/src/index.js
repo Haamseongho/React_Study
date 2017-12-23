dbf402fac1806c8df099a2b3c9c058a2aaa735b2
haamseongho <haams704@gmail.com>
2017-12-23 12:06:13 +0900

    state 내용 추가 (TIL)

---
+++ state/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Counter} from "./components/App";
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Counter/>, rootElement);
